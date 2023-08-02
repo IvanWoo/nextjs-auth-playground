4094c0fa5a5e2f04660686b3352afedff1f7401d
Yifan Wu <tyivanwu@gmail.com>
2023-08-02 16:47:25 -0400

    chore: add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  const url = request.url;
+  console.log("middleware =>", { url });
+  if (request.nextUrl.pathname.startsWith("/api/hello")) {
+    console.log("middleware => you are in /api/hello");
+    return NextResponse.next();
+  } else {
+    return NextResponse.next();
+  }
+}
+
+export const config = {
+  matcher: ["/api/:path*"],
+};
