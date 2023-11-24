09d2b0e54bc9487a2f53a8783264e19ec3f81782
Yifan Wu <tyivanwu@gmail.com>
2023-11-24 13:25:47 -0500

    chore: add test for wildcard user

--- tests/openfga.sh
+++ tests/openfga.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
+set -x
 
 store_id="$OPENFGA_STORE_ID"
 
 fga query check --store-id "$store_id" user:foo@example.com writer document:bar
 
 fga tuple write --store-id "$store_id" user:foo@example.com owner document:bar
+fga tuple write --store-id "$store_id" "user:*" reader document:public
 
+fga query check --store-id "$store_id" user:foo@example.com can_read document:public
 fga query check --store-id "$store_id" user:foo@example.com can_read document:bar
 fga query check --store-id "$store_id" user:foo@example.com can_write document:bar
 
